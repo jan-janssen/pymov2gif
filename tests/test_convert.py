a684982f5daece368184cadd930ce38d8ff68570
Jan Janßen <janssen@mpie.de>
2025-08-01 21:39:09 +0200

    file path

--- tests/test_convert.py
+++ tests/test_convert.py
@@ -5,7 +5,8 @@ from unittest import TestCase
 
 class TestConvert(TestCase):
     def test_convert(self):
-        convert("white.mov", resolution='20x20', framerate=10, output_file=None)
+        input_file = os.path.join(os.path.dirname(__file__), "white.mov")
+        convert(input_file, resolution='20x20', framerate=10, output_file=None)
         self.assertTrue(os.path.exists("white.gif"))
 
     def tearDown(self):
